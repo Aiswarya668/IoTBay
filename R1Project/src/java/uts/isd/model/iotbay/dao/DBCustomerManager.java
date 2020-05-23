125d505ea501bd941bd9bf453122a0bacac157d2
kevinleung56 <kevinleung56@gmail.com>
2020-05-23 15:58:53 +1000

    add more crud operations to customer manager

--- R1Project/src/java/uts/isd/model/iotbay/dao/DBCustomerManager.java
+++ R1Project/src/java/uts/isd/model/iotbay/dao/DBCustomerManager.java
@@ -41,13 +41,10 @@ public class DBCustomerManager {
                 String customerCity = rs.getString(8);
                 String customerState = rs.getString(9);
                 String customerPostC = rs.getString(10);
-//                String customerLogin = rs.getString(11);
-//                String customerRegD = rs.getString(12);
                 String customerGender = rs.getString(13);
                 return new Customer(customerFname, customerLname, customerEmail,
                     customerPass, customerGender,  customerUnit, customerSAdd, 
                     customerCity, customerState, customerPostC, customerPhone);
-//                    customerLogin,customerRegD);
             }
         }
         return null;
@@ -91,28 +88,64 @@ public class DBCustomerManager {
     }
     
     //Update (Update a Customer's details in the database)
-    public void updateCustomer(String contactName, String CustomerName, String CustomerEmail, String CustomerAddress) throws SQLException {
-        st.executeUpdate("UPDATE IOTBAYUSER.Customer SET CONTACTNAME='" + contactName + "', COMPANYNAME='" + CustomerName + "', COMPANYADDRESS='" + CustomerAddress +"' WHERE COMPANYEMAIL='"+ CustomerEmail +"' )");
+    public void updateCustomer(String customerFname, String customerLname, 
+            String customerEmail, String customerPass, String customerGender,
+            String customerUnit, String customerSAdd, String customerCity, 
+            String customerState, String customerPostC, String customerPhone)
+            throws SQLException {
+        String query = "UPDATE IOTBAYUSER.Customer SET CUSTOMEREMAIL = ?, "
+                + "FNAME = ?, LNAME = ?, PHONENUMBER = ?, PASSWORD = ?,"
+                + "STREETADDRESS = ?, UNITNUMBER = ?, CITY = ?, STATE = ?,"
+                + "POSTALCODE = ?, GENDER = ?";
+        
+        PreparedStatement stmt = conn.prepareStatement(query);
+        stmt.setString(1, customerEmail);
+        stmt.setString(2, customerFname);
+        stmt.setString(3, customerLname);
+        stmt.setString(4, customerPhone);
+        stmt.setString(5, customerPass);
+        stmt.setString(6, customerSAdd);
+        stmt.setString(7, customerUnit);
+        stmt.setString(8, customerCity);
+        stmt.setString(9, customerState);
+        stmt.setString(10, customerPostC);
+        stmt.setString(11, customerGender);
+        
+        stmt.executeUpdate();
     }
     
     //Delete (Delete a Customer from the database)
     public void deleteCustomer(String CustomerEmail) throws SQLException {
-        st.executeUpdate("DELETE FROM IOTBAYUSER.Customer WHERE EMAIL='" + CustomerEmail + "'");
+        String query = "DELETE FROM IOTBAYUSER.Customer WHERE EMAIL = ?";
+        PreparedStatement stmt = conn.prepareStatement(query);
+        stmt.setString(1, CustomerEmail);
+        
+        stmt.executeUpdate();
     }
     
-    public ArrayList<Customer> fectCustomer() throws SQLException {
-    String fetch = "select * from Customer";
-    ResultSet rs = st.executeQuery(fetch);
-    ArrayList<Customer> temp = new ArrayList();
+    public ArrayList<Customer> fetchCustomers() throws SQLException {
+    String query = "SELECT * FROM Customer";
+    ResultSet rs = st.executeQuery(query);
+    ArrayList<Customer> result = new ArrayList();
     
     while (rs.next()) {
-        String contactName = rs.getString(1);
-        String CustomerName = rs.getString(2);
-        String CustomerEmail = rs.getString(3);
-        String CustomerAddress = rs.getString(4);
-//        temp.add(new Customer(contactName, CustomerName, CustomerEmail, CustomerAddress));
+        String customerEmail = rs.getString(1);
+        String customerFname = rs.getString(2);
+        String customerLname = rs.getString(3);
+        String customerPhone = rs.getString(4);
+        String customerPass = rs.getString(5);
+        String customerSAdd = rs.getString(6);
+        String customerUnit = rs.getString(7);
+        String customerCity = rs.getString(8);
+        String customerState = rs.getString(9);
+        String customerPostC = rs.getString(10);
+        String customerGender = rs.getString(13);
+        result.add(new Customer(customerFname, customerLname, customerEmail,
+                    customerPass, customerGender,  customerUnit, customerSAdd, 
+                    customerCity, customerState, customerPostC, customerPhone));
     }
-    return temp;
+    
+    return result;
 }
     
     public boolean checkCustomer(String contactName, String CustomerName) throws SQLException {
