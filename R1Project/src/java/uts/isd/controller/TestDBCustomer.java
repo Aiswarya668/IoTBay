ce4ebd8662e85ac0fb2d6ab409b662f0b0776745
kevinleung56 <kevinleung56@gmail.com>
2020-05-30 09:36:07 +1000

    typo

--- R1Project/src/java/uts/isd/controller/TestDBCustomer.java
+++ R1Project/src/java/uts/isd/controller/TestDBCustomer.java
@@ -173,7 +173,7 @@ public class TestDBCustomer {
                 System.out.printf("%-40s %-20s %-20s %-20s %-30s %-20s %-10s %-20s %-20s %-10s %-10s %-20s %-10s %-10s \n",
                         customer.getEmail(), customer.getFirstName(), customer.getLastName(), customer.getPhoneNumber(),
                         customer.getPassword(), customer.getStreetAddress(), customer.getUnitNumber(),
-                        customer.getCity(), customer.getState(), customer.getPostcode(), customer.isLoginStatus(),
+                        customer.getCity(), customer.getState(), customer.getPostcode(), customer.getLoginStatus(),
                         formatter.format(customer.getDateRegistered()), customer.getGender(), customer.getActive());
             });
             System.out.println();
