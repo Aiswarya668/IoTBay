e322c1d21762ce6796d02c72e31773a3035e84b5
kevinleung56 <kevinleung56@gmail.com>
2020-05-29 21:50:30 +1000

    more progress on edit servlet

--- R1Project/src/java/uts/isd/controller/EditServlet.java
+++ R1Project/src/java/uts/isd/controller/EditServlet.java
@@ -28,54 +28,126 @@ public class EditServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
-        // 1- retrieve the current session
+        //1- retrieve the current session
         HttpSession session = request.getSession();
-        // 2- create an instance of the Validator class
+        //2- create an instance of the Validator class
         Validator validator = new Validator();
-        // 3- capture the posted email
+        //3- capture the posted parameters/info fields  
+        String firstName = request.getParameter("FirstName");
+        String lastName = request.getParameter("LastName");
         String email = request.getParameter("Email");
-        // 4- capture the posted password
         String password = request.getParameter("Password");
-        // 5- retrieve the manager instance from session
-        DBCustomerManager customerManager = (DBCustomerManager) session.getAttribute("customerManager");
-
+        String gender = request.getParameter("gender");
+        String unitNumber = request.getParameter("UnitNumber");
+        String streetAddress = request.getParameter("StreetAddress");
+        String city = request.getParameter("City");
+        String state = request.getParameter("State");
+        String postCode = request.getParameter("PostCode");
+        String phoneNumber = request.getParameter("PhoneNumber");
+        //5- retrieve the manager instance from session
+        DBCustomerManager customerManager = (DBCustomerManager)session.getAttribute("customerManager");
+            
         Customer customer = null;
         validator.clear(session);
-
+        
         try {
             customer = customerManager.findCustomer(email);
         } catch (SQLException ex) {
             Logger.getLogger(LoginServlet.class.getName()).log(Level.SEVERE, null, ex);
         }
-
-        if (!validator.validateEmail(email)) {
-            // set incorrect email error to the session
+        
+        if (customer != null) {
+             // set duplicate email error to the session 
+            session.setAttribute("existErr", "Customer with that email already exists in the database");
+            // redirect user to the login.jsp to retry
+            request.getRequestDispatcher("register.jsp").include(request, response);
+        }
+        else if (!validator.validateEmail(email)) {
+            // set incorrect email error to the session 
             session.setAttribute("emailErr", "Error: Email format incorrect");
-            // redirect user back to the login.jsp
-            request.getRequestDispatcher("login.jsp").include(request, response);
-        } else if (!validator.validatePassword(password)) {
-            // set incorrect password error to the session
-            session.setAttribute("passErr", "Error: Password format incorrect: min. 4 characters");
-            // redirect user back to the login.jsp
-            request.getRequestDispatcher("login.jsp").include(request, response);
-        } else if (customer != null) {
-            if (customer.getPassword().equals(password)) {
-                // save the logged in user object to the session
-                session.setAttribute("customer", customer);
-                // redirect user to the main.jsp
-                request.getRequestDispatcher("welcome.jsp").include(request, response);
+            // redirect user back to the login.jsp     
+            request.getRequestDispatcher("register.jsp").include(request, response);
+        } 
+        else if (!validator.validatePassword(password)) {
+            // set incorrect password error to the session 
+            session.setAttribute("passErr", "Error: Must be at least 4 characters long");
+            // redirect user back to the login.jsp 
+            request.getRequestDispatcher("register.jsp").include(request, response);
+        } 
+        else if (!validator.validateSingleString(firstName)) {
+            // set incorrect email error to the session 
+            session.setAttribute("fNameErr", "Error: First name is mandatory");
+            // redirect user back to the login.jsp     
+            request.getRequestDispatcher("register.jsp").include(request, response);
+        } 
+        else if (!validator.validateSingleString(lastName)) {
+            // set incorrect email error to the session 
+            session.setAttribute("lNameErr", "Error: Last name is mandatory");
+            // redirect user back to the login.jsp     
+            request.getRequestDispatcher("register.jsp").include(request, response);
+        } 
+        else if (!validator.validateSingleInt(unitNumber)) {
+            // set incorrect email error to the session 
+            session.setAttribute("unitErr", "Error: Unit number is mandatory");
+            // redirect user back to the login.jsp     
+            request.getRequestDispatcher("register.jsp").include(request, response);
+        } 
+        else if (gender == null) {
+            // set incorrect email error to the session 
+            session.setAttribute("genderErr", "Error: Gender is mandatory");
+            // redirect user back to the login.jsp     
+            request.getRequestDispatcher("register.jsp").include(request, response);
+        }
+        else if (!validator.validateSingleString(streetAddress)) {
+            // set incorrect email error to the session 
+            session.setAttribute("streetErr", "Error: Street address is mandatory");
+            // redirect user back to the login.jsp     
+            request.getRequestDispatcher("register.jsp").include(request, response);
+        } 
+        else if (!validator.validateSingleString(city)) {
+            // set incorrect email error to the session 
+            session.setAttribute("emailErr", "Error: City is mandatory");
+            // redirect user back to the login.jsp     
+            request.getRequestDispatcher("cityErr.jsp").include(request, response);
+        } 
+        else if (!validator.validateSingleString(state)) {
+            // set incorrect email error to the session 
+            session.setAttribute("stateErr", "Error: State is mandatory");
+            // redirect user back to the login.jsp     
+            request.getRequestDispatcher("register.jsp").include(request, response);
+        } 
+        else if (!validator.validateSingleInt(postCode)) {
+            // set incorrect email error to the session 
+            session.setAttribute("postErr", "Error: Post code is mandatory");
+            // redirect user back to the login.jsp     
+            request.getRequestDispatcher("register.jsp").include(request, response);
+        } 
+        else if (phoneNumber == null) {
+            // set incorrect phone number error to the session 
+            session.setAttribute("phoneErr", "Error: Phone number is mandatory");
+            // redirect user back to the login.jsp     
+            request.getRequestDispatcher("register.jsp").include(request, response);
+        }
+        else if (!validator.validatePhone(phoneNumber)) {
+            // set incorrect phone number error to the session 
+            session.setAttribute("phoneErr", "Error: Phone number format is incorrect");
+            // redirect user back to the login.jsp     
+            request.getRequestDispatcher("register.jsp").include(request, response);
+        } 
+        else {
+            // create new user
+            try {
+                customerManager.addCustomer(firstName, lastName, email, 
+                password, gender, unitNumber, streetAddress, city, 
+                state, postCode, phoneNumber);
             }
-            else {
-                // set incorrect password error to the session
-                session.setAttribute("passErr", "Error: Incorrect password");
-                // redirect user back to the login.jsp
+            catch (SQLException ex) {
+                // exception message if adding customer fails
+                session.setAttribute("exceptionErr", "Registration failed");
                 request.getRequestDispatcher("login.jsp").include(request, response);
             }
-        } else {
-            // set user does not exist error to the session
-            session.setAttribute("existErr", "Customer does not exist in the database");
-            // redirect user back to the login.jsp
-            request.getRequestDispatcher("login.jsp").include(request, response);
+            // redirect new user to the welcome.jsp
+            request.getRequestDispatcher("welcome.jsp").include(request, response);
         }
     }
 }
--- R1Project/web/edit.jsp
+++ R1Project/web/edit.jsp
@@ -25,43 +25,40 @@
         <div class="maincolumn2">
             <div class="card">
                 <h1>Edit profile <span> <%= (updated != null) ? updated : ""%> </span></h1>
-                <form method="post" method="get" action="edit.jsp">
+                <form method="post" method="get" action="EditServlet">
                     <table>
-                        <tr><td>Email</td><td><input type="text" value="${customer.email}" name="Email"></td></tr>
-                        <tr><td>First Name</td><td><input type="text" value="${customer.firstName}" name="FirstName"></td></tr>
-                        <tr><td>Last Name</td><td><input type="text" value="${customer.lastName}" name="LastName"></td></tr>
-                        <tr><td>Password</td><td><input type="password" value="${customer.password}" name="Password"></td></tr>
-                        <tr><td>Gender</td><td><input type="radio" id="Male" value="${customer.gender}" name="gender" value="Male"> <label for="Male">Male</label><br> <input type="radio" id="Female" name="gender" value="Female"><label for="Female">Female</label><br></td></tr>
-                        <tr><td>Phone Number</td><td><input type="text" value="${customer.phoneNumber}" name="PhoneNumber"></td></tr>   
-                        <tr><td>Unit Number</td><td><input type="text" value="${customer.unitNumber}" name="UnitNumber"></td></tr>
-                        <tr><td>Street Address</td><td><input type="text" value="${customer.streetAddress}" name="StreetAddress"></td></tr>
-                        <tr><td>City</td><td><input type="text" value="${customer.city}" name="City"></td></tr>
-                        <tr><td>State</td><td><input type="text" value="${customer.state}" name="State"></td></tr>
-                        <tr><td>Postal Code</td><td><input type="text" value="${customer.postcode}" name="PostCode"></td></tr>
+                        <tr><td>Email</td><td><input type="text" value="${customer.email}" 
+                                name="Email"></td></tr>
+                        <tr><td>First Name</td><td><input type="text" value="${customer.firstName}" 
+                                name="FirstName"></td></tr>
+                        <tr><td>Last Name</td><td><input type="text" value="${customer.lastName}" 
+                                name="LastName"></td></tr>
+                        <tr><td>Password</td><td><input type="password" value="${customer.password}" 
+                                name="Password"></td></tr>
+                        <tr><td>Gender</td><td><input type="radio" id="Male" value="${customer.gender}" 
+                                name="gender" value="Male"> 
+                        <label for="Male">Male</label>
+                        <br> 
+                        <input type="radio" id="Female" name="gender" value="Female">
+                        <label for="Female">Female</label><br></td></tr>
+                        <tr><td>Phone Number</td><td><input type="text" value="${customer.phoneNumber}" 
+                                name="PhoneNumber"></td></tr>   
+                        <tr><td>Unit Number</td><td><input type="text" value="${customer.unitNumber}" 
+                                name="UnitNumber"></td></tr>
+                        <tr><td>Street Address</td><td><input type="text" value="${customer.streetAddress}" 
+                                name="StreetAddress"></td></tr>
+                        <tr><td>City</td><td><input type="text" value="${customer.city}" 
+                                name="City"></td></tr>
+                        <tr><td>State</td><td><input type="text" value="${customer.state}" 
+                                name="State"></td></tr>
+                        <tr><td>Postal Code</td><td><input type="text" value="${customer.postcode}" 
+                                name="PostCode"></td></tr>
                     </table>
                     <div>
                         <input type ="hidden" name="updated" value="Update was successful">
                         <input class ="button3" type="submit" value="Update"</a>
                     </div>
                 </form>
-
-                <%
-                    if (updated != null) {
-                        String firstName = request.getParameter("FirstName");
-                        String lastName = request.getParameter("LastName");
-                        String email = request.getParameter("Email");
-                        String password = request.getParameter("Password");
-                        String gender = request.getParameter("gender");
-                        String unitNumber = request.getParameter("UnitNumber");
-                        String streetAddress = request.getParameter("StreetAddress");
-                        String city = request.getParameter("City");
-                        String state = request.getParameter("State");
-                        String postCode = request.getParameter("PostCode");
-                        String phoneNumber = request.getParameter("PhoneNumber");
-                        
-                        session.setAttribute("user", customer);
-                    }
-                %>
             </div>
         </div>
 
