f1bb1ec00c0243ab1fecdc8d541de511fe734b8a
seant1 <sean.tran@outlook.com.au>
2020-05-31 16:04:58 +1000

    Refactor Customer boolean getter to correct naming convention

--- R1Project/src/java/uts/isd/controller/TestDBCustomer.java
+++ R1Project/src/java/uts/isd/controller/TestDBCustomer.java
@@ -173,8 +173,8 @@ public class TestDBCustomer {
                 System.out.printf("%-40s %-20s %-20s %-20s %-30s %-20s %-10s %-20s %-20s %-10s %-10s %-20s %-10s %-10s \n",
                         customer.getEmail(), customer.getFirstName(), customer.getLastName(), customer.getPhoneNumber(),
                         customer.getPassword(), customer.getStreetAddress(), customer.getUnitNumber(),
-                        customer.getCity(), customer.getState(), customer.getPostcode(), customer.getLoginStatus(),
-                        formatter.format(customer.getDateRegistered()), customer.getGender(), customer.getActive());
+                        customer.getCity(), customer.getState(), customer.getPostcode(), customer.isLoginStatus(),
+                        formatter.format(customer.getDateRegistered()), customer.getGender(), customer.isActive());
             });
             System.out.println();
         } catch (SQLException ex) {
