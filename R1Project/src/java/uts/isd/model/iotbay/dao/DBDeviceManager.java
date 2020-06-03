d5374e3b99c7ee9336dce8e6821cf2f372257be8
seant1 <sean.tran@outlook.com.au>
2020-06-03 17:30:42 +1000

    Fix errors from merge

--- R1Project/src/java/uts/isd/model/iotbay/dao/DBDeviceManager.java
+++ R1Project/src/java/uts/isd/model/iotbay/dao/DBDeviceManager.java
@@ -28,20 +28,29 @@ public class DBDeviceManager {
         while (rs.next()){
             String searchedDeviceName = rs.getString(2);
             String searchedType = rs.getString(3);
-           
-                if(searchedDeviceName.equals(deviceName) && searchedType.equals(type)){
-                    int searchedDeviceID = rs.getInt(1);
-                    String newSearchedDeviceName = rs.getString(2);
-                    String newsearchedType = rs.getString(3);
-                    double searchedDeviceCost = rs.getDouble(4);
-                    int searchedDeviceStock = rs.getInt(5);
-                    String searchedDeviceDescription = rs.getString(6);
+
+            if (searchedDeviceName.equals(deviceName) && searchedType.equals(type)) {
+                int searchedDeviceID = rs.getInt(1);
+                String newSearchedDeviceName = rs.getString(2);
+                String newsearchedType = rs.getString(3);
+                double searchedDeviceCost = rs.getDouble(4);
+                int searchedDeviceStock = rs.getInt(5);
+                String searchedDeviceDescription = rs.getString(6);
+
+                if (searchedDeviceName.equals(deviceName) && searchedType.equals(type)) {
+                    searchedDeviceID = rs.getInt(1);
+                    //String searchedDeviceName = rs.getString(2);
+                    //String searchedType = rs.getString(3);
+                    searchedDeviceCost = rs.getDouble(4);
+                    searchedDeviceStock = rs.getInt(5);
+                    searchedDeviceDescription = rs.getString(6);
 
                     //return device searched from query 
-                    return new Device (searchedDeviceID, searchedDeviceName, searchedType, searchedDeviceCost, searchedDeviceStock, searchedDeviceDescription);
+                    return new Device(searchedDeviceID, searchedDeviceName, searchedType, searchedDeviceCost, searchedDeviceStock, searchedDeviceDescription);
                 }
             }
-        //if not found - return null
+            //if not found - return null
+        }
         return null;
         }
         
@@ -61,9 +70,9 @@ public class DBDeviceManager {
         
         stmt.executeUpdate();
     }
-    
-   //Update device details 
-    public void updateDevice(int deviceID, String deviceName, String type, double cost, int stockQuantity, String description) throws SQLException{
+
+    //Update device details 
+    public void updateDevice(int deviceID, String deviceName, String type, double cost, int stockQuantity, String description) throws SQLException {
         st.executeUpdate("UPDATE IOTBAYUSER.Device SET DEVICENAME='" + deviceName + "', TYPE='" + type + "', COST=" + cost + ", STOCKQUANTITY=" + stockQuantity + ", DESCRIPTION='" + description + "' WHERE DEVICEID=" + deviceID + "");
     }
     
@@ -73,28 +82,28 @@ public class DBDeviceManager {
     public void deleteDevice(int deviceID) throws SQLException{
         st.executeUpdate("DELETE FROM IOTBAYUSER.Device WHERE DEVICEID=" + deviceID + "");
     }
-    
-    public Device selectAll() throws SQLException{
+
+    public Device selectAll() throws SQLException {
         String fetch = "SELECT * FROM DEVICE";
         return null;
     }
-    
+
     //Fetch all devices 
     public ArrayList<Device> fetchDevice()throws SQLException{
         String fetch = "SELECT * FROM DEVICE";
         ResultSet rs = st.executeQuery(fetch);
         ArrayList<Device> result = new ArrayList();
-        
-            while (rs.next()){
-                int searcheddeviceID = rs.getInt(1);
-                String searcheddeviceName = rs.getString(2);
-                String searchedtype = rs.getString(3);
-                double searchedcost = rs.getDouble(4);
-                int searchedstockQuantity = rs.getInt(5);
-                String searcheddescription = rs.getString(6);
-                result.add(new Device(searcheddeviceID, searcheddeviceName, searchedtype, searchedcost, searchedstockQuantity, searcheddescription));
-            }
-            
+
+        while (rs.next()) {
+            int searcheddeviceID = rs.getInt(1);
+            String searcheddeviceName = rs.getString(2);
+            String searchedtype = rs.getString(3);
+            double searchedcost = rs.getDouble(4);
+            int searchedstockQuantity = rs.getInt(5);
+            String searcheddescription = rs.getString(6);
+            result.add(new Device(searcheddeviceID, searcheddeviceName, searchedtype, searchedcost, searchedstockQuantity, searcheddescription));
+        }
+
         return result;
     }
     
@@ -112,34 +121,32 @@ public class DBDeviceManager {
         }
         return false;
     }
-    
+
     //for update find by device ID 
-    
-    public Device findDeviceID(int deviceID) throws SQLException{
-        
+    public Device findDeviceID(int deviceID) throws SQLException {
+
         //Searches device by ID and puts in result set - rs enables iterative reading 
         String query = "SELECT * FROM IOTBAYUSER.DEVICE WHERE DEVICEID=" + deviceID + "";
         // results added to result set 
         ResultSet rs = st.executeQuery(query);
-        
-        while (rs.next()){
-           int searchedDeviceID = rs.getInt(1);
-            
-                if(searchedDeviceID == deviceID){
-                    
-                    String searchedDeviceName = rs.getString(2);
-                    String searchedType = rs.getString(3);
-                    double searchedDeviceCost = rs.getDouble(4);
-                    int searchedDeviceStock = rs.getInt(5);
-                    String searchedDeviceDescription = rs.getString(6);
 
-                    //return device searched from query 
-                    return new Device (searchedDeviceID, searchedDeviceName, searchedType, searchedDeviceCost, searchedDeviceStock, searchedDeviceDescription);
-                }
+        while (rs.next()) {
+            int searchedDeviceID = rs.getInt(1);
+
+            if (searchedDeviceID == deviceID) {
+
+                String searchedDeviceName = rs.getString(2);
+                String searchedType = rs.getString(3);
+                double searchedDeviceCost = rs.getDouble(4);
+                int searchedDeviceStock = rs.getInt(5);
+                String searchedDeviceDescription = rs.getString(6);
+
+                //return device searched from query 
+                return new Device(searchedDeviceID, searchedDeviceName, searchedType, searchedDeviceCost, searchedDeviceStock, searchedDeviceDescription);
             }
+        }
         //if not found - return null
         return null;
-        }
-  
-    
+    }
+
 }
